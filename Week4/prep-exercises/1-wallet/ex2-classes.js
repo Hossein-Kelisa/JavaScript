0776dc18b0f5ef62441ed332780e6640b6c9cb12
Hossein <Hossein.kelisa@gmail.com>
2024-12-12 12:35:52 +0100

    complete x2-classes

--- Week4/prep-exercises/1-wallet/ex2-classes.js
+++ Week4/prep-exercises/1-wallet/ex2-classes.js
@@ -3,14 +3,26 @@ import eurosFormatter from './euroFormatter.js';
 class Wallet {
   #name;
   #cash;
+  #dailyAllowance;
+  #dayTotalWithdrawals;
 
   constructor(name, cash) {
     this.#name = name;
     this.#cash = cash;
+    this.#dailyAllowance = 40;  // Default value for daily withdrawal limit
+    this.#dayTotalWithdrawals = 0;  // Total withdrawals for the day
   }
 
   get name() {
     return this.#name;
+  }
+
+  get dailyAllowance() {
+    return this.#dailyAllowance;
+  }
+
+  get dayTotalWithdrawals() {
+    return this.#dayTotalWithdrawals;
   }
 
   deposit(amount) {
@@ -23,7 +35,13 @@ class Wallet {
       return 0;
     }
 
+    if (this.#dayTotalWithdrawals + amount > this.#dailyAllowance) {
+      console.log(`Daily withdrawal limit exceeded!`);
+      return 0;
+    }
+
     this.#cash -= amount;
+    this.#dayTotalWithdrawals += amount;
     return amount;
   }
 
@@ -41,6 +59,18 @@ class Wallet {
     console.log(
       `Name: ${this.name}, balance: ${eurosFormatter.format(this.#cash)}`
     );
+    console.log(`Daily Withdrawn: ${eurosFormatter.format(this.#dayTotalWithdrawals)}`);
+    console.log(`Remaining Daily Allowance: ${eurosFormatter.format(this.#dailyAllowance - this.#dayTotalWithdrawals)}`);
+  }
+
+  resetDailyAllowance() {
+    this.#dayTotalWithdrawals = 0;
+    console.log(`Daily withdrawals reset for ${this.name}.`);
+  }
+
+  setDailyAllowance(newAllowance) {
+    this.#dailyAllowance = newAllowance;
+    console.log(`New daily withdrawal limit set for ${this.name}: ${eurosFormatter.format(this.#dailyAllowance)}`);
   }
 }
 
@@ -49,15 +79,25 @@ function main() {
   const walletJoe = new Wallet('Joe', 10);
   const walletJane = new Wallet('Jane', 20);
 
+  // Perform transactions
   walletJack.transferInto(walletJoe, 50);
   walletJane.transferInto(walletJoe, 25);
-
   walletJane.deposit(20);
   walletJane.transferInto(walletJoe, 25);
 
   walletJack.reportBalance();
   walletJoe.reportBalance();
   walletJane.reportBalance();
+
+  // Update daily allowance
+  walletJack.setDailyAllowance(60);
+  walletJack.withdraw(40); // This should be allowed
+  walletJack.withdraw(25); // This should exceed the limit
+  walletJack.reportBalance();
+
+  // Reset daily allowance
+  walletJack.resetDailyAllowance();
+  walletJack.reportBalance();
 }
 
 main();
