d7b2f51af192080f713ee57c362bf31d5a221673
Hossein <Hossein.kelisa@gmail.com>
2024-12-12 12:38:40 +0100

    complete ex5-prototype

--- Week4/prep-exercises/1-wallet/ex5-prototype.js
+++ Week4/prep-exercises/1-wallet/ex5-prototype.js
@@ -3,6 +3,8 @@ import eurosFormatter from './euroFormatter.js';
 function Wallet(name, cash) {
   this._name = name;
   this._cash = cash;
+  this._dailyAllowance = 40;  // Default daily withdrawal limit
+  this._dayTotalWithdrawals = 0;  // Total withdrawn today
 }
 
 Wallet.prototype.deposit = function (amount) {
@@ -15,7 +17,13 @@ Wallet.prototype.withdraw = function (amount) {
     return 0;
   }
 
+  if (this._dayTotalWithdrawals + amount > this._dailyAllowance) {
+    console.log(`Daily withdrawal limit exceeded!`);
+    return 0;
+  }
+
   this._cash -= amount;
+  this._dayTotalWithdrawals += amount;
   return amount;
 };
 
@@ -33,10 +41,32 @@ Wallet.prototype.reportBalance = function () {
   console.log(
     `Name: ${this._name}, balance: ${eurosFormatter.format(this._cash)}`
   );
+  console.log(
+    `Daily Withdrawn: ${eurosFormatter.format(this._dayTotalWithdrawals)}`
+  );
+  console.log(
+    `Remaining Daily Allowance: ${eurosFormatter.format(
+      this._dailyAllowance - this._dayTotalWithdrawals
+    )}`
+  );
 };
 
 Wallet.prototype.getName = function () {
   return this._name;
+};
+
+Wallet.prototype.resetDailyAllowance = function () {
+  this._dayTotalWithdrawals = 0;
+  console.log(`Daily withdrawals reset for ${this._name}.`);
+};
+
+Wallet.prototype.setDailyAllowance = function (newAllowance) {
+  this._dailyAllowance = newAllowance;
+  console.log(
+    `New daily withdrawal limit set for ${this._name}: ${eurosFormatter.format(
+      this._dailyAllowance
+    )}`
+  );
 };
 
 function main() {
@@ -53,6 +83,16 @@ function main() {
   walletJack.reportBalance();
   walletJoe.reportBalance();
   walletJane.reportBalance();
+
+  // Update daily allowance
+  walletJack.setDailyAllowance(60);
+  walletJack.withdraw(40); // This should be allowed
+  walletJack.withdraw(25); // This should exceed the limit
+  walletJack.reportBalance();
+
+  // Reset daily allowance
+  walletJack.resetDailyAllowance();
+  walletJack.reportBalance();
 }
 
 main();
